aec30142638fe27eb1bf8f886a7d56ce01240aae
priolette <volungevicius.adomas@gmail.com>
2022-09-10 19:50:08 +0300

    Initial commit

--- Energy Saver/Pages/Index.cshtml.cs
+++ Energy Saver/Pages/Index.cshtml.cs
@@ -13,13 +13,13 @@ namespace Energy_Saver.Pages
         }
 
         [BindProperty(SupportsGet = true)]
-        public string FirstName { get; set; }
+        public string City { get; set; }
 
         public void OnGet()
         {
-            if (string.IsNullOrWhiteSpace(FirstName))
+            if (string.IsNullOrWhiteSpace(City))
             {
-                FirstName = "User";
+                City = "The Web";
             }
         }
     }

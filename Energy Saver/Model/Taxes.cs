cfe2fec84a7021df427b831a56d50bce18102395
Tauras Rimdžius <tvikis3223@gmail.com>
2022-09-17 15:45:34 +0300

    Taxes.cs and resolving program.cs

---
+++ Energy Saver/Model/Taxes.cs
@@ -0,0 +1,13 @@
+﻿namespace Energy_Saver.Model
+{
+    public class Taxes
+    {
+
+        public int Id { get; set; }
+        public string Month { get; set; }
+        public int  GasPrice{ get; set; }
+        public int ElectricityPrice { get; set; }
+        public int WaterPrice { get; set; }
+        public int HeatingPrice { get; set; }
+    }
+}

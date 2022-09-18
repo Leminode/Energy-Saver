e6e74a113f1e7c5b160f10c720d928e1f55b41cd
priolette <volungevicius.adomas@gmail.com>
2022-09-18 14:39:18 +0300

    Update Index.cshtml and Index.csthml.cs to read JSON file

--- Energy Saver/Pages/Index.cshtml.cs
+++ Energy Saver/Pages/Index.cshtml.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Energy_Saver.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Energy_Saver.Pages
 {
     public class IndexModel : PageModel
     {
-        [BindProperty]
-        public Records Records { get; set; }
         private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public List<Taxes> Taxes { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -16,6 +20,13 @@ namespace Energy_Saver.Pages
         
 
         public void OnGet()
+        {
+            string path = "Resources/Taxes.json";
+            string json = System.IO.File.ReadAllText(Path.GetFullPath(path));
+            Taxes = JsonConvert.DeserializeObject<List<Taxes>>(json);
+        }
+
+        public void OnPost()
         {
 
         }

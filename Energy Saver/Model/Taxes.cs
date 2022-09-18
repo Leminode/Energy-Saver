7764702d509ba784a51703b7418bfa45d075e171
Tauras Rimdžius <tvikis3223@gmail.com>
2022-09-18 17:29:56 +0300

    Changing taxes.cs types to float

--- Energy Saver/Model/Taxes.cs
+++ Energy Saver/Model/Taxes.cs
@@ -6,14 +6,19 @@ namespace Energy_Saver.Model
     {
         [JsonProperty("id")]
         public int Id { get; set; }
+
         [JsonProperty("month")]
         public string Month { get; set; }
+
         [JsonProperty("gasPrice")]
         public float GasPrice { get; set; }
+
         [JsonProperty("electricityPrice")]
         public float ElectricityPrice { get; set; }
+
         [JsonProperty("waterPrice")]
-        public int WaterPrice { get; set; }
+        public float WaterPrice { get; set; }
+
         [JsonProperty("heatingPrice")]
         public float HeatingPrice { get; set; }
         

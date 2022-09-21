d0c262e4d9a81917f5ddf5ce8461b81357c47a8b
Tauras Rimdžius <tvikis3223@gmail.com>
2022-09-21 13:19:15 +0300

    Removed data folder

--- Energy Saver/Pages/Input.cshtml.cs
+++ Energy Saver/Pages/Input.cshtml.cs
@@ -5,19 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Energy_Saver.Data;
 using Energy_Saver.Model;
 
 namespace Energy_Saver.Pages
 {
     public class InputModel : PageModel
     {
-        private readonly Energy_Saver.Data.Energy_SaverContext _context;
-
-        public InputModel(Energy_Saver.Data.Energy_SaverContext context)
-        {
-            _context = context;
-        }
 
         public IActionResult OnGet()
         {
@@ -29,13 +22,10 @@ namespace Energy_Saver.Pages
        
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
-
-            _context.Taxes.Add(Taxes);
-            await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }

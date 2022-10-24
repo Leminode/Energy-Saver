f8e4295b07e079601552c61078022212cf67a28d
Tauras Rimdžius <tvikis3223@gmail.com>
2022-10-25 00:22:25 +0300

    Added tax feedback to home tab

--- Energy Saver/Program.cs
+++ Energy Saver/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddRazorPages(options =>
 
 builder.Services.AddTableServices();
 builder.Services.AddChartServices();
+builder.Services.AddSuggestionServices();
 
 var app = builder.Build();
 

06137e3afd2cb80e60369ba2af1d875653fd2e05
Tauras Rimdžius <tvikis3223@gmail.com>
2022-11-14 21:51:53 +0200

    Added TaxInputTest and TaxRemoveTest

--- Energy-Saver.Tests/UnitTest1.cs
+++ Energy-Saver.Tests/UnitTest1.cs
@@ -1,4 +1,6 @@
+using Energy_Saver.DataSpace;
 using Energy_Saver.Model;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace Energy_Saver.Tests
@@ -6,20 +8,59 @@ namespace Energy_Saver.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public async void TaxInputTest()
         {
-            var taxes = new Taxes
+            using (var db = new EnergySaverTaxesContext(Utilities.TestDbContextOptions()))
             {
-                ID = 1,
-                UserID = 4,
-                Month = Months.January,
-                GasAmount = 1,
-                ElectricityAmount = 2,
-                WaterAmount = 3,
-                HeatingAmount = 4
-            };
+                var taxes = new Taxes
+                {
+                    ID = 1,
+                    UserID = 4,
+                    Year = 2020,
+                    Month = Months.January,
+                    GasAmount = 1,
+                    ElectricityAmount = 2,
+                    WaterAmount = 3,
+                    HeatingAmount = 4
+                };
 
-            
+                List<Taxes> taxList = new List<Taxes>();
+                taxList.Add(taxes);
+
+                db.Taxes.Add(taxes);
+                await db.SaveChangesAsync();
+
+                var list = await db.Taxes.ToListAsync();
+
+                Assert.Equal(taxList, list);
+            }
+        }
+
+        [Fact]
+        public async void TaxRemoveTest()
+        {
+            using (var db = new EnergySaverTaxesContext(Utilities.TestDbContextOptions()))
+            {
+                var taxes = new Taxes
+                {
+                    ID = 1,
+                    UserID = 4,
+                    Year = 2020,
+                    Month = Months.January,
+                    GasAmount = 1,
+                    ElectricityAmount = 2,
+                    WaterAmount = 3,
+                    HeatingAmount = 4
+                };
+
+                db.Taxes.Add(taxes);
+                await db.SaveChangesAsync();
+
+                db.Taxes.Remove(taxes);
+                await db.SaveChangesAsync();
+
+                Assert.True(db.Taxes.Any());
+            }
         }
     }
 }
